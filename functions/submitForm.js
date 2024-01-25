dd96000926b42c2a76fefceabf668cf00043c8ba
FrederikG <fg@blackbird-media.dk>
2024-01-25 22:41:36 +0100

    Update submitForm.js

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -5,7 +5,14 @@ exports.handler = async function (event, context) {
   try {
     const { email } = JSON.parse(event.body);
 
+    // Log the received email
+    console.log('Received email:', email);
+
     const apiKey = process.env.API_KEY;
+
+    // Log the API key (for debugging)
+    console.log('API Key:', apiKey);
+
     const url = 'https://api.sendgrid.com/v3/marketing/contacts';
     const listId = 'c35ce8c7-0b05-4686-ac5c-67717f5e5963'; // Replace with your list ID
 
