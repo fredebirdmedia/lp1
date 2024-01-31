d10268522c2c86c16fb88cca2db7e0eb67acc9e7
FrederikG <fg@blackbird-media.dk>
2024-01-31 15:35:08 +0100

    ui

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -1,17 +1,13 @@
-const axios = require('axios');
-
 exports.handler = async function (event, context) {
   try {
     if (event.httpMethod !== 'POST') {
-      // Ensure that only POST requests are processed
       return {
         statusCode: 405,
         body: JSON.stringify({ error: 'Method Not Allowed' })
       };
     }
 
-    const { email, phone_number, first_name } = JSON.parse(event.body);
-
+    const { email, phone_number, first_name } = event.queryStringParameters;
 
     // Access the environment variable directly
     const apiKey = process.env.API_KEY;
@@ -21,19 +17,19 @@ exports.handler = async function (event, context) {
     }
 
     const url = 'https://api.sendgrid.com/v3/marketing/contacts';
-    const listId = 'c35ce8c7-0b05-4686-ac5c-67717f5e5963'; // Replace with your list ID
+    const listId = 'c35ce8c7-0b05-4686-ac5c-67717f5e5963';
 
     const data = {
- contacts: [
-      {
-        email: email
-        phone_number: "phone_number"
-        first_name: "first_name"
-        
-      }
-    ],
-  list_ids: [listId]
-};
+      contacts: [
+        {
+          email: email,
+          country: "CA",
+          phone_number: phone_number,
+          first_name: first_name
+        }
+      ],
+      list_ids: [listId]
+    };
 
     const options = {
       method: 'POST',
@@ -41,18 +37,13 @@ exports.handler = async function (event, context) {
         'Authorization': `Bearer ${apiKey}`,
         'Content-Type': 'application/json'
       },
-      data: data // Include the data field in the axios request
+      data: data
     };
 
-    // Log before making the request
     console.log('Making Axios request with the following options:', options);
 
     const response = await axios.post(url, {}, options);
 
-    console.log('Axios response:', response);
-
-
-    // Log after a successful response
     console.log('Axios request successful. Response:', response.data);
 
     return {
