dd52d38be8f6adaffbed0e1fb312349c917d70e3
fredebirdmedia <157717544+fredebirdmedia@users.noreply.github.com>
2024-02-28 12:57:50 +0100

    Add files via upload

--- netlify/functions/submitFormm.js
+++ netlify/functions/submitFormm.js
@@ -2,58 +2,32 @@ const axios = require('axios');
 
 exports.handler = async function (event, context) {
   try {
-    if (event.httpMethod !== 'POST') {
-      // Ensure that only POST requests are processed
-      return {
-        statusCode: 405,
-        body: JSON.stringify({ error: 'Method Not Allowed' })
-      };
-    }
-
     const { email, phone_number } = JSON.parse(event.body);
 
-
-    // Access the environment variable directly
     const apiKey = process.env.API_KEY;
-
-    if (!apiKey) {
-      throw new Error('API_KEY is not defined');
-    }
-
     const url = 'https://api.sendgrid.com/v3/marketing/contacts';
     const listId = 'c35ce8c7-0b05-4686-ac5c-67717f5e5963'; // Replace with your list ID
 
     const data = {
- contacts: [
-      {
-        email: email,
-        phone_number: phone_number
-      
-        
-        
-      }
-    ],
-  list_ids: [listId]
-};
+       contacts: [{ email: email, phone_number: phone_number }],
+      list_ids: [listId]
+    };
 
     const options = {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Authorization': `Bearer ${apiKey}`,
         'Content-Type': 'application/json'
       },
-      data: data // Include the data field in the axios request
+      data: JSON.stringify(data)
     };
 
     // Log before making the request
     console.log('Making Axios request with the following options:', options);
 
-    const response = await axios.post(url, data, options);
+    const response = await axios.put(url, options.data, { headers: options.headers });
 
-    console.log('Axios response:', response);
-
-
-    // Log after a successful response
+    // Log after successful response
     console.log('Axios request successful. Response:', response.data);
 
     return {
