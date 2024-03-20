5984b5736aea5b72bc37e4b4adc193d5c4842dde
fredebirdmedia <157717544+fredebirdmedia@users.noreply.github.com>
2024-03-20 16:21:56 +0100

    Add files via upload

--- netlify/functions/submitForm3.js
+++ netlify/functions/submitForm3.js
@@ -68,7 +68,7 @@ exports.handler = async function (event, context) {
     if (phone_number) {
       const simpleTextingData = {
         contactPhone: phone_number,
-        lists: [{
+        listIds: [{
           //id: '65d60667f82cb04ba121461f',
           name: 'CA'
         }]
