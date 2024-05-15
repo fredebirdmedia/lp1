685dd5c9aa1172a7e6a9418243480a6d41ebbac0
fredebirdmedia <fg@blackbird-media.dk>
2024-05-15 11:58:41 +0200

    stsats updated

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,21 +1,37 @@
-// fetchData.js
-
-const fetch = require('node-fetch');
-
-exports.handler = async (event, context) => {
-    const { startDate, endDate } = JSON.parse(event.body);
-
+// Update the fetchData function to make a request to your Netlify Function
+async function fetchData(startDate, endDate) {
     try {
-        const response = await fetch(`https://www.buffalopartners.com/api/campaignfeed?username=blackbirdmedia&apikey=44DB33F3-37CE-4A09-87A3-40A0FAE787B6&startdate=${startDate}&enddate=${endDate}`);
+        const response = await fetch('/.netlify/functions/fetchData', {
+            method: 'POST',
+            body: JSON.stringify({ startDate, endDate })
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/xml')) {
+            throw new Error('Response is not in XML format');
+        }
+
         const data = await response.text();
-        return {
-            statusCode: 200,
-            body: data
-        };
+
+        // Check if the response is not empty
+        if (data.trim() === '') {
+            throw new Error('Empty response received');
+        }
+
+        // Parse XML data
+        let parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(data, "text/xml");
+
+        // Convert XML data to HTML string for display
+        let html = xmlDoc.documentElement.outerHTML;
+
+        // Display XML data in the campaign-feed div
+        document.getElementById('campaign-feed').innerHTML = html;
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch data from buffalopartners.com' })
-        };
+        console.error('Error fetching XML:', error);
     }
-};
+}
