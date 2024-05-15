aeb1e1514857153201c9857ffa71d101f4ea6684
fredebirdmedia <fg@blackbird-media.dk>
2024-05-15 12:22:11 +0200

    stats updated

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,9 +1,6 @@
 // Define the fetchData function
-async function fetchData(event, context) {
+async function fetchData(startDate, endDate) {
     try {
-        const startDate = event.startDate;
-        const endDate = event.endDate;
-
         // Your existing logic for fetching data goes here
         const response = await fetch(`https://www.buffalopartners.com/api/campaignfeed?username=blackbirdmedia&apikey=44DB33F3-37CE-4A09-87A3-40A0FAE787B6&startdate=${startDate}&enddate=${endDate}`);
 
@@ -36,5 +33,17 @@ async function fetchData(event, context) {
     }
 }
 
-// Export the fetchData function
-module.exports = { fetchData };
+// Handler function for incoming requests
+exports.handler = async function(event, context) {
+    try {
+        const { startDate, endDate } = JSON.parse(event.body);
+        const result = await fetchData(startDate, endDate);
+        return result;
+    } catch (error) {
+        console.error('Error handling request:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
+};
