027d8f26b1e5f89d0e09d54ed2463bc96185b306
fredebirdmedia <fg@blackbird-media.dk>
2025-07-02 15:50:24 +0200

    SAcommiformt

---
+++ netlify/functions/submitFormZA.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  try {
+    const { email, phone_number } = JSON.parse(event.body);
+
+    // Handling request related to SendGrid
+    const apiKey = process.env.API_KEY;
+    const sendgridUrl = 'https://api.sendgrid.com/v3/marketing/contacts';
+    const sendgridListId = 'fbeae2e7-5aef-40c3-8afa-a9de61ba0e79'; // Replace with your SendGrid list ID
+
+    const sendgridData = {
+      contacts: [{
+        email: email,
+        phone_number: phone_number
+      }],
+      list_ids: [sendgridListId]
+    };
+
+    const sendgridOptions = {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(sendgridData)
+    };
+
+    console.log('Making Axios request to SendGrid with the following options:', sendgridOptions);
+
+    const sendgridResponse = await axios.put(sendgridUrl, sendgridOptions.data, { headers: sendgridOptions.headers });
+
+    console.log('SendGrid request successful. Response:', sendgridResponse.data);
+
+ 
+};
